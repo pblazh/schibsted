7bddfdf348a4d75e628e7c96f6110d756cea4736
Pawel Blazejewski <pawel.blazejewski@grandparade.co.uk>
2019-02-09 21:37:40 +0100

    feat: added docker

--- packages/server/src/app.js
+++ packages/server/src/app.js
@@ -1,10 +1,12 @@
 import express from 'express'
+import cors from "cors";
 import bodyParser from 'body-parser'
 import issuesRouter from './issueTracker'
 
 const app = express()
 app.set('port', process.env.PORT || 5000)
 app.use(bodyParser.json())
+app.use(cors())
 app.use('/api', issuesRouter)
 
 export default app
